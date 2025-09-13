024053223c8c54caf31ddd22aff73583f8b83be5
Rayen Belfayez <119884263+uzi-belfayez@users.noreply.github.com>
2025-09-13 15:57:36 +0200

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -43,6 +43,14 @@ const Projects = () => {
       githubUrl: "https://github.com/uzi-belfayez/Pricer-Agent"
     },
     {
+      title: "ROS2-Mobile-Robot-Platform",
+      description: "ROS2-based mobile robot platform designed for both simulation and real-world robotics applications.",
+      image: realshitImage,
+      technologies: ["ROS2", "Navigation Stack", "RViz", "URDF/XACRO", "Python", "Cmake"],
+      liveUrl: "#",
+      githubUrl: "https://github.com/uzi-belfayez/ROS2-Mobile-Robot-Platform"
+    },
+      {
       title: "CO2, Temperature, and Humidity Monitoring System with Presence Detection",
       description: "A FreeRTOS monitoring system.",
       image: realshitImage,
